cf735f5d77d402f05c456e587ad02cffc26fb004
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2026-01-14 13:03:14 +0530

    solved 677 A vanya and fence problem

---
+++ 677A_vanya_fence.py
@@ -0,0 +1,12 @@
+def main():
+  n,h = list(map(int,input().split()))
+  friends = list(map(int,input().split()))
+  width = 0
+  for friend in friends:
+    if friend > h:
+      width += 2
+    else:
+      width += 1
+  print(width)
+if __name__ == "__main__":
+  main()
