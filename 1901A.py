2958b0c0bc3b0c5bfa586f0aadbf0713dcad70ac
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2026-03-15 01:54:56 +0530

    fix(1901A.py) : refactored

--- 1901A.py
+++ 1901A.py
@@ -1,21 +1,22 @@
+def procedure(n: int, x: int, stations: list[int]) -> int:
+  prev =  0
+  mx_gap = 0
+
+  for station in stations:
+    mx_gap = max(mx_gap, station - prev)
+    prev = station
+  
+  return max(mx_gap, 2 * (x - stations[-1]))
+
+
 def main():
   t = int(input())
 
   for _ in range(t):
     n, x = list(map(int, input().split()))
     stations = list(map(int,input().split()))
-
-    prev = 0
-    mx_gap = 0
-
-    for s in stations:
-      mx_gap = max(mx_gap, s - prev)
-      prev = s
-
-    mx = max(mx_gap, 2 * (x - stations[-1]))
-
-    print(mx)
-
+    res = procedure(n, x, stations)
+    print(res)
 if __name__ == "__main__":
   main()
   
