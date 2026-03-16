654c07d501b770ab7c4c47efb6ddf0089c4b7702
Shyamanth-2005 <shyamanthreddy.m@gmail.com>
2026-03-16 18:45:46 +0530

    feat(472A.py): solve 472 A problem

---
+++ 472A.py
@@ -0,0 +1,26 @@
+def is_prime(n: int) -> bool:
+  if n <= 1: return False
+  if n == 2: return True
+
+  for i in range(2, int(n ** 0.5) + 1):
+    if n % i == 0:
+      return False
+  return True
+
+
+def main():
+  n = int(input())
+
+  for i in range(1,n):
+    if i == 1:
+      continue
+    x = i
+    y = n - i
+    if not is_prime(x) and not is_prime(y):
+      print(x,y)
+      break
+
+    
+
+if __name__ == "__main__":
+  main()
